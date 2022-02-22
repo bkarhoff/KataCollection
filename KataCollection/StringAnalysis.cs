6a5a1b7b33b3d78baa8d67a15d13ba21202495e8
Benjamin W. Karhoff <benjamin@electricquilt.com>
2022-02-22 09:12:56 -0500

    Add project files.

---
+++ KataCollection/StringAnalysis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataCollection {
+    public class StringAnalysis {
+        /// <summary>
+        /// Count the number of Duplicates
+        /// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. 
+        /// The input string can be assumed to contain only alphabets(both uppercase and lowercase) and numeric digits.
+        /// 
+        /// Source: codewars.com
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int DuplicateCount(string str) {
+            string lowerStr = str.ToLower();
+            List<char> foundChars = new List<char>();
+            List<char> dupChars = new List<char>();
+            int count = 0;
+
+            foreach(char i in lowerStr) {
+                if(foundChars.Contains(i) && !dupChars.Contains(i)) {
+                    count++;
+                    dupChars.Add(i);
+                } else {
+                    foundChars.Add(i);
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ KataTests/StringAnalysisTests.cs
@@ -0,0 +1,19 @@
+﻿using KataCollection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests {
+    public class StringAnalysisTests {
+        [Test]
+        public void KataTests() {
+            Assert.AreEqual(0, StringAnalysis.DuplicateCount(""));
+            Assert.AreEqual(0, StringAnalysis.DuplicateCount("abcde"));
+            Assert.AreEqual(2, StringAnalysis.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, StringAnalysis.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, StringAnalysis.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, StringAnalysis.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+    }
+}
