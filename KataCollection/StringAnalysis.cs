4323505740f6a915389f81d2037510d89b726167
Benjamin W. Karhoff <benjamin@electricquilt.com>
2022-03-29 09:10:52 -0400

    Added Extract

--- KataCollection/StringAnalysis.cs
+++ KataCollection/StringAnalysis.cs
@@ -55,6 +55,35 @@ namespace KataCollection {
             }
             return orderedList;
         }
-
+        /// <summary>
+        /// A format for expressing an ordered list of integers is to use a comma separated list of either
+        /// individual integers
+        /// or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. 
+        /// The range includes all integers in the interval including both endpoints.It is not considered a range unless it spans 
+        /// at least 3 numbers.For example "12,13,15-17"
+        /// Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+        /// 
+        /// Example:
+        ///     solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+        ///     returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+        /// </summary>
+        public static string Extract(int[] args) {
+            string result = "";
+            for(int i = 0; i < args.Length; i++) {
+                result += args[i];
+                if(i < args.Length - 1 && args[i] != args[i + 1] - 1) {
+                    result += ",";
+                } else if(i < args.Length - 2 && args[i] != args[i + 2] - 2) {
+                    result += ",";
+                } else {
+                    i++;
+                    while( i < args.Length - 1 && args[i] == args[i - 1] + 1 && args[i] == args[i + 1] - 1) {
+                        i++;
+                    }
+                    result += $"-{args[i]},";
+                }
+            }
+            return result; 
+        }
     }
 }
--- KataTests/StringAnalysisTests.cs
+++ KataTests/StringAnalysisTests.cs
@@ -20,5 +20,20 @@ namespace KataTests {
             Assert.AreEqual("", StringAnalysis.UniqueInOrder(""));
             Assert.AreEqual("ABCDAB", StringAnalysis.UniqueInOrder("AAAABBBCCDAABBB"));
         }
+        [Test]
+        public void ExtractTests() {
+            Assert.AreEqual("1,2", StringAnalysis.Extract(new[] { 1, 2 }));
+            Assert.AreEqual("1-3", StringAnalysis.Extract(new[] { 1, 2, 3 }));
+
+            Assert.AreEqual(
+                "-6,-3-1,3-5,7-11,14,15,17-20",
+                StringAnalysis.Extract(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 })
+            );
+
+            Assert.AreEqual(
+                "-3--1,2,10,15,16,18-20",
+                StringAnalysis.Extract(new[] { -3, -2, -1, 2, 10, 15, 16, 18, 19, 20 })
+            );
+        }
     }
 }
